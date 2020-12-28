26de00fc2b861f985f1fbdb63cfd8685e7502d79
Polad Mirzayev <polad.mirzayev@gmail.com>
2020-12-28 13:35:53 -0700

    Update favicon and meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -18,7 +18,7 @@ export default function MyApp({ Component, pageProps }) {
   return (
     <React.Fragment>
       <Head>
-        <title>Mighty Kazan - Nomadic food</title>
+        <title>Mighty Kazan - Nomadic food of Central Asia</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
       </Head>
       <ThemeProvider theme={theme}>
