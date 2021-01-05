6a7d5455b21fec3b5e7ba1ce4a70de9062a10989
polad <polad@users.noreply.github.com>
2021-01-05 03:58:43 +0000

    deploy: 67fc06e0b93be927bf3d0094c5afa926b7f7f697

--- _next/static/gS__PJWRWNpYLL5836huB/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-8bac732a4636a8980003.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-b478f9e931dc75ea7fcf.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
