7a1115b1470bfe42e1f200bc55ea79ed67666138
Polad Mirzayev <polad.mirzayev@gmail.com>
2021-01-06 21:44:33 -0700

    Simplify scroll indicator

--- components/ScrollIndicator/index.js
+++ components/ScrollIndicator/index.js
@@ -1,6 +1,6 @@
 import ArrowDown from '@material-ui/icons/KeyboardArrowDown'
 import { makeStyles } from '@material-ui/core/styles'
-import { useEffect } from 'react'
+import { useState } from 'react'
 import { animated, useSpring } from 'react-spring'
 
 const useStyles = makeStyles(theme => ({
@@ -10,36 +10,31 @@ const useStyles = makeStyles(theme => ({
     textAlign: 'center',
     userSelect: 'none',
     color: 'white',
-    lineHeight: '0.2',
+    height: '2.5rem',
   },
   icon: {
     height: '3rem',
     width: '3rem',
-    marginTop: -6,
-  },
-  label: {
-    fontSize: '1.2rem',
   },
 }))
 
+const AnimatedArrowDown = animated(ArrowDown)
+
 export default function ScrollIndicator() {
   const classes = useStyles()
-  const [ styleProps, setStyleProps, stop ] = useSpring(() => ({
-    to: { lineHeight: 0.2 },
-    config: { friction: 30, tension: 200 }
-  }))
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setStyleProps({ lineHeight: styleProps.lineHeight.value === 0.2 ? 1 : 0.2 })
-    }, 200)
-    return () => clearInterval(intervalId)
+  const [ reset, setReset ] = useState(true)
+  const styleProps = useSpring({
+    to: {
+      opacity: reset ? 1 : 0.3,
+      transform: reset ? 'translateY(0px)' : 'translateY(-20px)'
+    },
+    config: { friction: 20, tension: 210 },
+    onRest: () => setReset(!reset),
   })
 
   return (
     <div className={classes.root}>
-      <animated.div style={styleProps} className={classes.label}>scroll</animated.div>
-      <ArrowDown className={classes.icon} />
+      <AnimatedArrowDown className={classes.icon} style={styleProps} />
     </div>
   )
 }
