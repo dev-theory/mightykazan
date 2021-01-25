f362e76f6fd438255b802466d9a04cdd6ea78a53
polad <polad@users.noreply.github.com>
2021-01-25 23:44:26 +0000

    deploy: c7e846be0abec9ab3d1a4fedaa68671e54271ad7

--- _next/static/2j5HHFwNzq2QVnXVqqB6g/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-92d48d0f6373e2fd4313.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-3b0252045cd6b1f14854.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/EbN6_uXjoJ8SgTCIGuzZk/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-0088d37b324d3710fd2f.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-3b0252045cd6b1f14854.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
